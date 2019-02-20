eeca9c28aad30a316e5d867e1409d54bf8d18b0c
thomasreindl <reindlthomas@gmx.at>
2019-02-20 16:08:26 +0100

    Unity Datei

---
+++ Assets/Scripts/HelloWeb.cs
@@ -0,0 +1,40 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class HelloWeb : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//}
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWeb : MonoBehaviour {
+	string helloweb = "HelloWeb";
+	
+	public void PrintHelloWeb()
+	{
+		Debug.Log(helloweb);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
