1e6392030ccc2439ba0fb8ef75ca8da344840483
thomasreindl <reindlthomas@gmx.at>
2019-02-27 17:43:51 +0100

    sceneswitcher added

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	public void LoadScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+}
+
